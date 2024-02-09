6e48d49d2a69a312a7f03691e0ad2f4b126418d8
kumaripriya-03 <159520814+kumaripriya-03@users.noreply.github.com>
2024-02-09 18:55:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function dop(){
+    var click=document.getElementById('drop-content');
+
+    if(click.style.display=="none")
+    {
+       click.style.display="block";
+    }
+    else{
+        click.style.display="none";
+    }
+}
